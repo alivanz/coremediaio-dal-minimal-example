7230a22e7f6f1b34fccb287e3d4b5860e4189168
alivan <alivan1627@gmail.com>
2021-01-27 18:16:54 +0700

    use semaphoore instead of sleep

--- lumina-control/main.m
+++ lumina-control/main.m
@@ -15,22 +15,31 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         NSXPCConnection *conn = [[NSXPCConnection alloc] initWithMachServiceName:serviceName options:0];
         conn.remoteObjectInterface = [NSXPCInterface interfaceWithProtocol:@protocol(assistanceProtocol)];
+        dispatch_semaphore_t sem0 = dispatch_semaphore_create(0);
+        dispatch_semaphore_t sem1 = dispatch_semaphore_create(0);
         conn.invalidationHandler = ^{
             NSLog(@"invalidate");
+            dispatch_semaphore_signal(sem0);
+            dispatch_semaphore_signal(sem1);
         };
         conn.interruptionHandler = ^{
             NSLog(@"interrupted");
+            dispatch_semaphore_signal(sem0);
+            dispatch_semaphore_signal(sem1);
         };
         NSLog(@"resuming %@", conn.serviceName);
         [conn resume];
         [conn.remoteObjectProxy upperCaseString:@"hello" withReply:^(NSString *result){
             NSLog(@"result %@", result);
+            dispatch_semaphore_signal(sem0);
         }];
         [conn.remoteObjectProxy getPixelBuffer:^(NSData *buffer, OSType type, size_t w, size_t h, size_t stride){
             NSLog(@"buffer length %lu", buffer.length);
             NSLog(@"%lux%lu", w, h);
+            dispatch_semaphore_signal(sem1);
         }];
-        [NSThread sleepForTimeInterval:0.5f];
+        dispatch_semaphore_wait(sem0, DISPATCH_TIME_FOREVER);
+        dispatch_semaphore_wait(sem1, DISPATCH_TIME_FOREVER);
     }
     return 0;
 }
